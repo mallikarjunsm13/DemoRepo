b29d11ef5a4f1bee919c3239ab3b819c6c0fa81b
Lenovo <mallumantagi@gmail.com>
2024-03-29 01:40:14 +0530

    commit

---
+++ DemoForSpringBoot/src/main/java/com/example/demo/DemoForSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForSpringBootApplication.class, args);
+		System.out.println("Hello Sringbooot");
+	}
+
+}
